6afb878e90f026adb8aa3d0f7c94bfa4245d02fe
bandvov <bandvov@gmail.com>
2024-05-26 10:06:54 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import HomePage from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Cars from "./components/cars/Cars";
+import Users from "./components/users/Users";
 
 function App() {
   return (
-    <div className="App">
-      <Cars />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/admin/cars" element={<Cars />} />
+        <Route path="/admin/users" element={<Users />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
