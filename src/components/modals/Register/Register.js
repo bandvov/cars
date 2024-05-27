56f6631e1f168f721f93850768fcf0619703c9c9
bandvov <bandvov@gmail.com>
2024-05-27 12:17:27 +0300

    update registration form

--- src/components/modals/Register/Register.js
+++ src/components/modals/Register/Register.js
@@ -27,70 +27,98 @@ const RegistrationForm = () => {
   });
 
   return (
-    <Formik
-      initialValues={{
-        first_name: "",
-        last_name: "",
-        login: "",
-        email: "",
-        password: "",
-        phone_number: "",
-      }}
-      validateOnBlur
-      validationSchema={validationSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        console.log(values);
-        setSubmitting(false);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form className="form-container">
-          <h1>Зареєструйтесь зараз!</h1>
-          <div className="form-group">
-            <label htmlFor="first_name">Імя</label>
-            <Field type="text" name="first_name" />
-            <ErrorMessage name="first_name" component="div" className="error" />
-          </div>
+    <div className="form-container">
+      <Formik
+        initialValues={{
+          first_name: "",
+          last_name: "",
+          login: "",
+          email: "",
+          password: "",
+          phone_number: "",
+        }}
+        validateOnBlur
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log(values);
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <h1>Зареєструйтесь зараз!</h1>
+            <div className="form-group">
+              <label htmlFor="first_name">Імя</label>
+              <Field type="text" name="first_name" />
+              <ErrorMessage
+                name="first_name"
+                component="div"
+                className="error"
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="last_name">Прізвище</label>
-            <Field type="text" name="last_name" />
-            <ErrorMessage name="last_name" component="div" className="error" />
-          </div>
+            <div className="form-group">
+              <label htmlFor="last_name">Прізвище</label>
+              <Field type="text" name="last_name" />
+              <ErrorMessage
+                name="last_name"
+                component="div"
+                className="error"
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="email">Пошта</label>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" className="error" />
-          </div>
+            <div className="form-group">
+              <label htmlFor="email">Пошта</label>
+              <Field type="email" name="email" />
+              <ErrorMessage name="email" component="div" className="error" />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="phone_number">Номер телефону</label>
-            <Field name="phone_number" type="number" />
-            <ErrorMessage
-              name="phone_number"
-              component="div"
-              className="error"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="login">Логін</label>
-            <Field name="login" type="text" />
-            <ErrorMessage name="login" component="div" className="error" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Пароль</label>
-            <Field type="password" name="password" />
-            <ErrorMessage name="password" component="div" className="error" />
-          </div>
+            <div className="form-group">
+              <label htmlFor="phone_number">Номер телефону</label>
+              <Field name="phone_number" type="number" />
+              <ErrorMessage
+                name="phone_number"
+                component="div"
+                className="error"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="login">Логін</label>
+              <Field name="login" type="text" />
+              <ErrorMessage name="login" component="div" className="error" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Пароль</label>
+              <Field type="password" name="password" />
+              <ErrorMessage name="password" component="div" className="error" />
+            </div>
 
-          <button type="submit" disabled={isSubmitting}>
-            Зареєструватись
-          </button>
-          <FacebookButton />
-        </Form>
-      )}
-    </Formik>
+            <button type="submit" disabled={isSubmitting}>
+              Зареєструватись
+            </button>
+            <FacebookButton />
+          </Form>
+        )}
+      </Formik>
+      <div style={{ display: "flex" }}>
+        <div
+          style={{
+            flex: 1,
+            borderBottom: "1px solid",
+            margin: "0 8px 8px 0",
+          }}
+        ></div>
+        <span>Або увійти через</span>
+        <div
+          style={{
+            flex: 1,
+            borderBottom: "1px solid",
+            margin: "0 0 8px 8px",
+          }}
+        ></div>
+      </div>
+      <FacebookButton />
+    </div>
   );
 };
 
