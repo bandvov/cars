fea650c3ee16797d64bdf8f750e319898f7ead05
bandvov <bandvov@gmail.com>
2024-05-28 08:07:00 +0300

    add username to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,55 @@
-import React from 'react';
-import './Header.css';
+import React, { useEffect } from "react";
+import "./Header.css";
+import { useAuth } from "../../providers/AuthProvider";
 
 const Header = () => {
+  const { user } = useAuth();
+
   return (
     <section className="header-section">
-    <header className="header">
-      <a href="/" className="logo">DriveSmart</a>
-      <nav className="main-nav">
-        <ul className="main-nav-list">
-          <li className="main-nav-list-item">
-            <a href="/Catalog" className="main-nav-link">Пропозиції</a>
-          </li>
-  
-          <li className="main-nav-list-item">
-            <a href="./page-3.html" className="main-nav-link">Мапа</a>
-          </li>
-  
-          <li className="main-nav-list-item">
-            <a href="./page-3.html" className="main-nav-link">Форум</a>
-          </li>
-  
-          <li>
-            <button type="button" className="header-btn">Завантажити додаток</button>
-          </li>
-  
-          <li className="main-nav-list-item">
-            <a href="/auth/login" className="main-nav-link">Увійти</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  </section>  
+      <header className="header">
+        <a href="/" className="logo">
+          DriveSmart
+        </a>
+        <nav className="main-nav">
+          <ul className="main-nav-list">
+            <li className="main-nav-list-item">
+              <a href="/Catalog" className="main-nav-link">
+                Пропозиції
+              </a>
+            </li>
+
+            <li className="main-nav-list-item">
+              <a href="./page-3.html" className="main-nav-link">
+                Мапа
+              </a>
+            </li>
+
+            <li className="main-nav-list-item">
+              <a href="./page-3.html" className="main-nav-link">
+                Форум
+              </a>
+            </li>
+
+            <li>
+              <button type="button" className="header-btn">
+                Завантажити додаток
+              </button>
+            </li>
+
+            <li className="main-nav-list-item">
+              {user ? (
+                <h3 className="main-nav-link">{user}</h3>
+              ) : (
+                <a href="/auth/login" className="main-nav-link">
+                  Увійти
+                </a>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </section>
   );
 };
 
